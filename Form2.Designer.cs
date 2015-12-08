e489284111f76583893ce393c2dbba790d6041cd
Matt Williamson <mattwilliamson@client-104-39-32-71.mobility-up.psu.edu>
2015-12-07 20:37:49 -0500

    Fixed form references, added speed input, calibration, and COM port functionality.

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -39,6 +39,19 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label11 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -111,9 +124,9 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(377, 306);
+            this.button4.Location = new System.Drawing.Point(402, 318);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(164, 88);
+            this.button4.Size = new System.Drawing.Size(164, 93);
             this.button4.TabIndex = 8;
             this.button4.Text = "Upload";
             this.button4.UseVisualStyleBackColor = true;
@@ -143,11 +156,144 @@
             this.listBox1.Size = new System.Drawing.Size(226, 173);
             this.listBox1.TabIndex = 11;
             // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 336);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(81, 20);
+            this.textBox3.TabIndex = 12;
+            this.textBox3.Text = "5";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 320);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Speed";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 359);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(99, 339);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(27, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "mph";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(154, 339);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(47, 17);
+            this.radioButton1.TabIndex = 16;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Auto";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(154, 363);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(60, 17);
+            this.radioButton2.TabIndex = 17;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Manual";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(151, 320);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(56, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Calibration";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(154, 386);
+            this.textBox4.MaxLength = 10;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 19;
+            this.textBox4.Text = "Latitude";
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(154, 412);
+            this.textBox5.MaxLength = 10;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 20;
+            this.textBox5.Text = "Longitude";
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(260, 376);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 13);
+            this.label9.TabIndex = 21;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(260, 409);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 13);
+            this.label10.TabIndex = 22;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(432, 46);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 23;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(429, 30);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(119, 13);
+            this.label11.TabIndex = 24;
+            this.label11.Text = "Select Radio Serial Port";
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(578, 442);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -183,5 +329,18 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label11;
     }
 }
--- Form2.cs
+++ Form2.cs
@@ -7,24 +7,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports;
 
 namespace SDCI
 {
     public partial class Form2 : Form
     {
-        int index = 0;      // number of waypoints
-        List<string> items = new List<string>();
+        List<string> items = new List<string>(); // holds the items in the waypoint log
+        int index = 0;                          // number of waypoints
+        bool validSpeed = true;                 // app starts with a valid default speed
+        bool validWaypointLog = false;          // are there any waypoints entered? if not, no upload allowed
+        //bool serialPortSelected = false;        // did user select serial port? if not, no upload allowed
+        bool calibrationSelected = false;       // did user select calibration? if not, no upload allwoed
 
         public Form2()
         {
             InitializeComponent();
-
             listBox1.DataSource = items;
         }
 
-        private void Form2_Load(object sender, EventArgs e)
+        private void Form2_Load(object sender, EventArgs e) // on form 2 startup
         {
-            button1.Enabled = false;
+            Variables.RefToForm2 = this;        // point the reference to form 2 at this form
+            
+            button1.Enabled = false;            // disable waypoint add until valid waypoint entered
+            button4.Enabled = false;            // disable Upload until waypoints entered
+
+            // This section of code prints all the computers COM ports in use into a drop down comboBox
+            // The user must then select which one the radio is using
+            string[] ArrayComPortsNames = null;
+            int index = -1;
+            string ComPortName = null;
+
+            ArrayComPortsNames = SerialPort.GetPortNames();
+            do
+            {
+                index += 1;
+                comboBox1.Items.Add(ArrayComPortsNames[index]);
+            }
+
+            while (!((ArrayComPortsNames[index] == ComPortName)
+                          || (index == ArrayComPortsNames.GetUpperBound(0))));
+            Array.Sort(ArrayComPortsNames);
+
+            //want to get first out
+            if (index == ArrayComPortsNames.GetUpperBound(0))
+            {
+                ComPortName = ArrayComPortsNames[0];
+            }
+            comboBox1.Text = ArrayComPortsNames[0];
         }
 
         private void Form2_FormClosing(object sender, FormClosingEventArgs e)   // if close (red x) is pressed
@@ -50,8 +81,20 @@ namespace SDCI
 
         private void button4_Click(object sender, EventArgs e)  // upload button
         {
-            Form3 f3 = new Form3();
-            f3.Show();
+            // add speed, calibration bytes, and waypoints to array (Variables.tripData) to be uploaded
+
+            Variables.ComPort.PortName = comboBox1.Text;        // set the COM port name for upload
+
+            if (Variables.RefToForm3 == null)
+            {
+                Form3 f3 = new Form3();
+                f3.Show();
+            }
+            else
+            {
+                Variables.RefToForm3.Show();
+            }
+
             this.Hide();
         }
 
@@ -68,9 +111,11 @@ namespace SDCI
             {
                 if ((n <= 180) && (n >= 0))
                 {
+                    double z;
+
                     label4.Text = "Longitude";
 
-                    if (double.TryParse(textBox1.Text, out n))
+                    if (double.TryParse(textBox1.Text, out z) && (z >= -90) && (z <= -65))
                     {
                         button1.Enabled = true;
                     }
@@ -93,9 +138,20 @@ namespace SDCI
 
             textBox1.Text = "Latitude";
             textBox2.Text = "Longitude";
+
+            validWaypointLog = true;            // at least one valid waypoint now
+
+            if (validSpeed && validWaypointLog && calibrationSelected)
+            {
+                button4.Enabled = true;
+            }
+            else
+            {
+                button4.Enabled = false;
+            }
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void textBox1_TextChanged(object sender, EventArgs e)   // latitude waypoint input
         {
             double n;
 
@@ -108,9 +164,11 @@ namespace SDCI
             {
                 if ((n <= -65) && (n >= -90))
                 {
+                    double z;
+
                     label3.Text = "Latitude";
 
-                    if (double.TryParse(textBox2.Text, out n))
+                    if (double.TryParse(textBox2.Text, out z) && (z >= 0) && (z <= 180))
                     {
                         button1.Enabled = true;
                     }
@@ -123,9 +181,8 @@ namespace SDCI
             }
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)  // The Remove button was clicked
         {
-            // The Remove button was clicked
             try
             {
                 // Remove the item in the List.
@@ -134,6 +191,21 @@ namespace SDCI
             }
             catch
             {
+            }
+
+            if (index < 1)                  // if last waypoint deleted, no waypoints to upload
+            {
+                validWaypointLog = false;
+                button4.Enabled = false;    // disable upload button
+            }
+
+            if (validSpeed && validWaypointLog && calibrationSelected)
+            {
+                button4.Enabled = true;
+            }
+            else
+            {
+                button4.Enabled = false;
             }
 
             listBox1.DataSource = null;
@@ -146,6 +218,7 @@ namespace SDCI
             int temp_index = index;
 
             for (int i = 0; i < temp_index; i++)
+            {
                 try
                 {
                     // Remove the item in the List.
@@ -155,9 +228,175 @@ namespace SDCI
                 catch
                 {
                 }
+            }
+
+            validWaypointLog = false;               // no longer any waypoints to upload
+            button4.Enabled = false;                // disable upload button
 
             listBox1.DataSource = null;
             listBox1.DataSource = items;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)   // speed input
+        {
+            double n;
+
+            if (!double.TryParse(textBox3.Text, out n)) // check if input isn't a number
+            {
+                label6.Text = "must be a number";
+                button4.Enabled = false;
+                validSpeed = false;
+            }
+            else
+            {
+                if ((n <= 20) && (n >= 0))
+                {
+                    label6.Text = "";
+                    validSpeed = true;
+                }
+                else
+                {
+                    label6.Text = "must be between\n0 and 20";
+                    button4.Enabled = false;
+                    validSpeed = false;
+                }
+            }
+
+            if (validSpeed && validWaypointLog && calibrationSelected)
+            {
+                button4.Enabled = true;
+            }
+            else
+            {
+                button4.Enabled = false;
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)   // auto calibration - send dummy bytes to onboard controls
+            {
+                textBox4.Text = "-100";       // dummy bytes must be something other than possible coordinates
+                textBox5.Text = "-100";
+                textBox4.Enabled = false;
+                textBox5.Enabled = false;
+                calibrationSelected = true;
+            }           // else manual calibration - allow user to enter basecamp coordinates
+            else
+            {
+                textBox4.Text = "Latitude";
+                textBox5.Text = "Longitude";
+                textBox4.Enabled = true;
+                textBox5.Enabled = true;
+
+                double n;
+
+                if (!(double.TryParse(textBox4.Text, out n) && (n <= -65) && (n >= -90))) // check if input isn't a number
+                {
+                    calibrationSelected = false;
+                }
+                else
+                {
+                    double z;
+
+                    if (double.TryParse(textBox5.Text, out z) && (z >= 0) && (z <= 180) && radioButton2.Checked)
+                    {
+                        calibrationSelected = true;
+                    }
+                    else
+                    {
+                        calibrationSelected = false;
+                    }
+                }
+            }
+            
+            if (validSpeed && validWaypointLog && calibrationSelected)
+            {
+                button4.Enabled = true;
+            }
+            else
+            {
+                button4.Enabled = false;
+            }
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)   // manual basecamp input (latitude)
+        {
+            double n;
+
+            if (!double.TryParse(textBox4.Text, out n)) // check if input isn't a number
+            {
+                label9.Text = "must be a number";
+                calibrationSelected = false;
+            }
+            else
+            {
+                if ((n <= -65) && (n >= -90))
+                {
+                    double z;
+
+                    label9.Text = "Latitude";
+
+                    if (double.TryParse(textBox5.Text, out z) && (z >= 0) && (z <= 180) && radioButton2.Checked)
+                    {
+                        calibrationSelected = true;
+                    }
+                }
+                else
+                {
+                    label9.Text = "must be between\n-65 and -90";
+                    calibrationSelected = false;
+                }
+            }
+
+            if (validSpeed && validWaypointLog && calibrationSelected)
+            {
+                button4.Enabled = true;
+            }
+            else
+            {
+                button4.Enabled = false;
+            }
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            double n;
+
+            if (!double.TryParse(textBox5.Text, out n)) // check if input isn't a number
+            {
+                label10.Text = "must be a number";
+                calibrationSelected = false;
+            }
+            else
+            {
+                if ((n <= 180) && (n >= 0))
+                {
+                    double z;
+
+                    label10.Text = "Longitude";
+
+                    if (double.TryParse(textBox4.Text, out z) && (z >= -90) && (z <= -65) && radioButton2.Checked)
+                    {
+                        calibrationSelected = true;
+                    }
+                }
+                else
+                {
+                    label10.Text = "must be between\n0 and 180";
+                    calibrationSelected = false;
+                }
+            }
+
+            if (validSpeed && validWaypointLog && calibrationSelected)
+            {
+                button4.Enabled = true;
+            }
+            else
+            {
+                button4.Enabled = false;
+            }
         }
     }
 }
